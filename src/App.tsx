80fee0254339cf05f0584746435f7c1ec9550914
wilmion <wilbert.navarrete@onroadts.com>
2023-05-02 15:32:42 -0500

    Connect data with Props

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useMemo, useState } from "react";
 
 import { PrimordialLayoutStyled } from "./styles/Layouts/primordial-layout.styled";
 
@@ -10,15 +10,27 @@ import { TableCustom } from "./components/Table";
 function App() {
   const [data, setData] = useState<string[][]>([[]]);
 
-  const bodyData = data.filter((_, i) => i !== 0);
+  const existCSV = useMemo(() => {
+    return data[0] && data[0].length > 0;
+  }, [data]);
+
+  const bodyData = useMemo(() => data.filter((_, i) => i !== 0), [data]);
+
+  const total: number = useMemo(() => {
+    if (!existCSV) return 0;
+
+    const salaries = bodyData.map((data) => Number(data[4]) || 0);
+
+    return salaries.reduce((s1, s2) => s1 + s2);
+  }, [bodyData, existCSV]);
 
   return (
     <div>
       <Header />
       <PrimordialLayoutStyled>
-        <TableActions onLoadCsv={setData} />
+        <TableActions onLoadCsv={setData} existCSV={existCSV} />
         <TableCustom headers={data[0]} bodyData={bodyData} />
-        <TableTotal />
+        <TableTotal total={total} />
       </PrimordialLayoutStyled>
     </div>
   );
