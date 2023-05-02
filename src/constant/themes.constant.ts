87abf07fe51d54c2c9de91b6667364fa5e58556f
wilmion <wilbert.navarrete@onroadts.com>
2023-05-02 12:25:24 -0500

    Set-up

---
+++ src/constant/themes.constant.ts
@@ -0,0 +1,11 @@
+import { createTheme } from "@material-ui/core";
+
+export const themeMui = createTheme({
+  typography: {
+    fontFamily: "Nunito, Arial",
+  },
+  palette: {
+    primary: { main: "#0a58ca" },
+    secondary: { main: "#1e2432" },
+  },
+});
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import { themeMui } from "./constant/themes.constant";
+
+import { ThemeProvider } from "styled-components";
+import { CssBaseline } from "@material-ui/core";
+import App from "./App";
+
+import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={themeMui}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
 
